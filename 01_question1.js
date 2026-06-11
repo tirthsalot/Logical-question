134a895558ec1c9d52ed6da5953d7a0da01be0de
tirthsalot <salottirth09@gmail.com>
2026-06-11 13:26:47 +0530

    Add files via upload

---
+++ 01_question1.js
@@ -0,0 +1,17 @@
+
+
+
+var num = 10;
+
+function execution(num){
+
+    const total = num*num;
+
+    return total;
+}
+
+let didgit1 = execution(num);
+
+let didgit2 = execution(num);
+
+console.log(didgit1,didgit2);
